d51f161f430ce2e3802a77e04e2b65f78d5dc671
Walter Cueva Chavez <waltercueva@gmail.com>
2022-07-08 17:11:45 -0500

    Update app.R

--- visualizacion/app.R
+++ visualizacion/app.R
@@ -1,5 +1,5 @@
 library(shiny)
-
+library(dplyr)
 source("www/presentacion.R")
 source("www/recoleccion.R")
 source("www/transformacion.R")
@@ -20,7 +20,50 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-
+  
+  
+  #####################################################Recoleccion###############################################
+  output$contents <- renderTable({
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+    
+    req(input$file1)
+    
+    # when reading semicolon separated files,
+    # having a comma separator causes `read.csv` to error
+    tryCatch(
+      {
+        df <- read.csv(input$file1$datapath,
+                       header = input$header)
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
+    
+  })
+  ##################################################### Fin Recoleccion ###############################################
+  ##################################################### transformacion ###############################################
+  dt<-read.csv("dataset/1.csv")
+  output$resDplyr<-renderDataTable({
+    selector<-input$transDplyr
+    if(is.null(selector)){
+      return (NULL)
+    }
+    if(selector=="1"){
+      return (dt)
+    }
+    if(selector=="2"){
+      return (dt%>%filter(C4>15))
+    }
+    if(selector=="3"){
+      return (dt%>%filter(grepl('Carla',Alumno)))
+    }
+    
+  })
 }
 
 
